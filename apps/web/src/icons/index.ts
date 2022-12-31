3d099d003a7e1b9e764284cf3bfaa5d87fa429a0
Irere Emmy <irere2020@outlook.com>
2022-12-31 14:33:07 +0200

    feat: added sidebar icons

--- apps/web/src/icons/index.ts
+++ apps/web/src/icons/index.ts
@@ -3,3 +3,5 @@ export { default as GithubIcon } from "./GithubIcon";
 export { default as TwitterIcon } from "./TwitterIcon";
 export { default as LgLogo } from "./LgLogo";
 export { default as FacebookIcon } from "./FacebookIcon";
+export { default as Logo } from "./Logo";
+export { default as PeopleIcon } from "./PeopleIcon";
--- apps/web/src/modules/layouts/Sidebar.tsx
+++ apps/web/src/modules/layouts/Sidebar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { HomeIcon } from "../../icons";
+import Link from "next/link";
+
+import { HomeIcon, Logo, PeopleIcon } from "../../icons";
 
 export const Sidebar: React.FC = () => {
   return (
-    <div>
-      <HomeIcon />
+    <div className="flex items-center flex-col pt-3">
+      <Link href={`/feed`}>
+        <Logo />
+      </Link>
+      <div className="flex flex-col items-center justify-center gap-3 pt-7 w-full">
+        <Link href={`/feed`}>
+          <HomeIcon />
+        </Link>
+        <Link href={`/people`}>
+          <PeopleIcon />
+        </Link>
+      </div>
     </div>
   );
 };
