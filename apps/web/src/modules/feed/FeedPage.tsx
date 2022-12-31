a82b0eb592196f53d17d8c2050b869ffa44de094
Irere Emmy <irere2020@outlook.com>
2022-12-31 13:50:05 +0200

    feat: added 404 page redirect

---
+++ apps/web/src/modules/feed/FeedPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HeaderController } from "../display/HeaderController";
+
+export const FeedPage = () => {
+  return (
+    <div>
+      <HeaderController embed={{}} title={`Dashboard`} />
+      <p>Feed</p>
+    </div>
+  );
+};
