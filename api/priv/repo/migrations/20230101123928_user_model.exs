0b915411b08031be963cd33a6bd5d2f50b14c4ef
Irere Emmy <irere2020@outlook.com>
2023-01-01 15:19:28 +0200

    feat(api): added the user schema and migrations

---
+++ api/priv/repo/migrations/20230101123928_user_model.exs
@@ -0,0 +1,34 @@
+defmodule Api.Repo.Migrations.UserModel do
+  use Ecto.Migration
+
+  def change do
+    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";", "")
+
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
+      add :username, :text, null: false
+      add :email, :text, null: true
+      add :schoolName, :text, null: true
+      add :bio, :text, default: ""
+      add :goal, :text, default: "friendship"
+      add :avatarUrl, :text, null: false
+      add :tokenVersion, :integer, default: 1
+      add :contributions, :integer, default: 0
+      add :numLikes, :integer, default: 0
+      add :numMatches, :integer, default: 0
+      add :birthday, :naive_datetime, null: false
+      add :online, :boolean, default: false
+      add :lastOnline, :naive_datetime
+      add :githubId, :text, null: true
+      add :githubAccessToken, :text, null: true
+      add :facebookId, :text, null: true
+      add :facebookAccessToken, :text, null: true
+      add :twitterId, :text, null: true
+      add :ip, :text, null: true
+      add :gender, :text, null: true
+      add :staff, :boolean, default: false
+
+      timestamps()
+    end
+  end
+end
