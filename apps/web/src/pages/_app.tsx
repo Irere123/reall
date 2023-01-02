0ff8ef4a82919818d64dffeab93147c42484cef7
Irere Emmy <irere2020@outlook.com>
2023-01-02 09:54:56 +0200

    feat(web): setup the app layout for ws

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import Head from "next/head";
+import ReactModal from "react-modal";
+import Router from "next/router";
+import NProgress from "nprogress";
 import { AppProps } from "next/app";
 
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
+import { WebSocketProvider } from "../modules/ws/WebSocketProvider";
+import { PageComponent } from "../types/PageComponent";
+import { isServer } from "../lib/isServer";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+ReactModal.setAppElement("#__next");
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  if (
+    isServer &&
+    !Component.getInitialProps &&
+    (Component as PageComponent<unknown>).ws
+  ) {
+    return null;
+  }
+
   return (
-    <>
+    <WebSocketProvider
+      shouldConnect={!!(Component as PageComponent<unknown>).ws}
+    >
       <Head>
         <link rel="icon" href="/reall_logo.svg" type="image/svg" />
         <link rel="manifest" href="/manifest.json" />
@@ -16,6 +41,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </WebSocketProvider>
   );
 }
