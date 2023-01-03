0234d7a6b0f8180b5e279998b05754ff243b5719
Irere Emmy <irere2020@outlook.com>
2023-01-03 11:09:29 +0200

    feat(api): added view db model and migrations

---
+++ api/priv/repo/migrations/20230103085601_view_model.exs
@@ -0,0 +1,15 @@
+defmodule Api.Repo.Migrations.ViewModel do
+  use Ecto.Migration
+
+  def change do
+    create table(:views, primary_key: false) do
+      add :liked, :boolean, default: false
+      add :shared, :boolean, default: false
+
+      add :viewerId, references(:users, type: :uuid, on_delete: :delete_all), null: false, primary_key: true
+      add :targetId, references(:users, type: :uuid, on_delete: :delete_all), null: false, primary_key: true
+
+      timestamps()
+    end
+  end
+end
